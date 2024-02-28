a43b5bfc006f48c74f0693a6c3b73fe2960bb941
rcm141 <48158893+rcm141@users.noreply.github.com>
2024-02-28 14:48:06 -0500

    Add files via upload
    Prints all JSON from Network in Chrome DevTools.
    Problems: 
       All files print to same file.
       Each file is on one line of JSON.

--- json reader.py
+++ json reader.py
@@ -8,29 +8,24 @@ from time import sleep
 
 BASE_URL = "https://www.fussball.de/homepage#!/"
 
-service = Service(executable_path='./chromedriver.exe')
-driver = webdriver.ChromeOptions()
-
-driver.set_capability("loggingPrefs", {"performance": "ALL"})
-
-driver = webdriver.Remote(
-    service=service,options=driver
-)
+options = Options()
+options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+driver = webdriver.Chrome(options=options)
 
 driver.get(BASE_URL)
 sleep(5)
 
 logs_raw = driver.get_log("performance")
+# Logs is mostly Headers and Initiators from Netowrk in DevTools
 logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
 
 def log_filter(log_):
     return (
-        log_["method"] == "Network.responseReceived"
+        log_["method"] == "Network.responseReceived" 
         and "json" in log_["params"]["response"]["mimeType"]
     )
 
 for log in filter(log_filter, logs):
     request_id = log["params"]["requestId"]
     resp_url = log["params"]["response"]["url"]
-    print(f"Caught {resp_url}")
-    print(driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id}))
+    open(str(resp_url)[-6:], "w").write(json.dumps(driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})),indent=4)
